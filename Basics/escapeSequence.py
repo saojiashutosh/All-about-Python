77b3fa89d0f2b774dfa8bbabe1412b466dcaf6f8
Ashutosh Saoji <ashutoshsaoji1@gmail.com>
2024-03-01 21:15:10 +0530

    commit

---
+++ Basics/escapeSequence.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print("This is Ashutosh")
+
+
+print("Hello World\nThis is Ashutosh")
+
+
+print("Hello World\tThis is Ashutosh")
+
+print("Hello World \"This is Ashutosh\"")
+
+print('Hello World \'This is Ashutosh\'')
