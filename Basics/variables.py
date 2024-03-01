c9e2835239736dee327414418c13358316492ccd
Ashutosh Saoji <ashutoshsaoji1@gmail.com>
2024-03-01 22:54:23 +0530

    commit

---
+++ Basics/variables.py
@@ -0,0 +1,21 @@
+# x = 5 
+# print(type(x))
+
+name = "ashutosh" 
+age = 24
+gender = "male"
+# letter = 'a'
+
+# Method 1 
+
+print("my name is ", name)
+print("my age is ",age)
+print("my gender is ",gender)
+
+print("my name is ",name,"my age is ",age,"my gender is ",gender)
+
+# method 2
+print(f"my name is {name}")
+print(f"my age is {age}")
+print(f"my gender is {gender}")
+print(f"my name is {name} my age is {age} my gender is {gender}")
