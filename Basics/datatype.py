303fc0e2bce062fca484335127061eb04e32ea06
Ashutosh Saoji <ashutoshsaoji1@gmail.com>
2024-03-03 22:10:54 +0530

    commit

---
+++ Basics/datatype.py
@@ -0,0 +1,13 @@
+x= 123
+y = "hello World"
+z = True
+a = [1,2,3,4,5]
+b= {1: "ashutosh" , 2 : "atharv" , 3 : "rohit" }
+c=(1,23,34,"hello")
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
