8ab7c79dd5d6a23a646bccfc58b15569ccff9ab3
Ashutosh Saoji <ashutoshsaoji1@gmail.com>
2024-03-03 23:28:25 +0530

    commit

--- Basics/datatype.py
+++ Basics/datatype.py
@@ -5,9 +5,9 @@ a = [1,2,3,4,5]
 b= {1: "ashutosh" , 2 : "atharv" , 3 : "rohit" }
 c=(1,23,34,"hello")
 
-print(type(x))
-print(type(y))
-print(type(z))
-print(type(a))
-print(type(b))
-print(type(c))
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
---
+++ Basics/type_casting.py
@@ -0,0 +1,25 @@
+x = 1233
+y= 4567
+
+z = x+y
+print(z)
+
+a= str(x)
+b= str(y)
+c= a+b
+print(c)
+
+# m = "2345"
+# n = "4567"
+
+# z=int(m+n)
+# print(z)
+
+
+m = "2345.6"
+n = "4567.9"
+
+z=float(m)
+o=float(n)
+p=z+o
+print(p)
