943c909f3abdd15e0e1c9230828740c5896b89a5
Ashutosh Saoji <ashutoshsaoji1@gmail.com>
2024-03-05 17:44:27 +0530

    commit

---
+++ Basics/arithmetic_and_assignment_operators.py
@@ -0,0 +1,22 @@
+# Arithmetic Operators 
+# Addition = +
+# Substrtaction = -
+# Multiplication = *
+# Division (Quotient) = / 
+# Mod (Reminder) = %
+# Exponential(Power) = **
+# Floor Division = // #14.44 --> 14
+
+x = 5
+y = 3 
+
+print(x+y) # Addition
+print(x-y) # Substrtaction
+print(x*y) # Multiplication
+print(x/y) # Division
+print(x%y) # Mod (Reminder)
+print(x**y) # Exponential(Power)
+print(x//y) # Floor Division 
+
+# Assignment Operator 
+# = , +=,-=,*=,/=,%=,//=,**=
